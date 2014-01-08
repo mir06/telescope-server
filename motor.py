a0d96603d36a6591658b04050a6db9a8312e0c1b
Armin Leuprecht <mir@mur.at>
2014-01-08 22:44:05 +0100

    first implementation of motor class and telescope server

---
+++ motor.py
@@ -0,0 +1,80 @@
+import time
+import RPi.GPIO as GPIO
+
+class Motor(object):
+    def __init__(self, name, pins, min_angle=0, max_angle=360):
+        self.name = name
+        self.PUL, self.DIR, self.ENBL = pins
+        self._steps_per_rev = 0
+        self._enabled = True
+        self._angle = None
+        self._min_angle = min_angle
+        self._max_angle = max_angle
+        for p in pins:
+            GPIO.setup(p, GPIO.OUT)
+            GPIO.output(p, True)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def steps_per_rev(self):
+        return self._steps_per_rev
+
+    @steps_per_rev.setter
+    def steps_per_rev(self, value):
+        self._steps_per_rev = value
+
+    @property
+    def enable(self):
+        return self._enabled
+
+    @enable.setter
+    def enable(self, enabled=True):
+        self._enabled = enabled
+        GPIO.output(self.ENBL, enabled)
+
+    @property
+    def angle(self):
+        return self._angle
+
+    @angle.setter
+    def angle(self, value):
+        self._angle = value % 360
+
+    @property
+    def calibrated(self):
+        return (self._angle != None) and (self._steps_per_rev>0)
+
+    def step(self, steps, direction):
+        GPIO.output(self.DIR, direction)
+        for step in xrange(steps):
+            GPIO.output(self.PUL, True)
+            GPIO.output(self.PUL, False)
+            time.sleep(.05)
+
+    def move(self, angle):
+        angle = angle % 360
+        if self._angle != None and self._steps_per_rev and self._enabled:
+            angle_to_move = (angle - self._angle) % 360
+            if angle_to_move > 180:
+                angle_to_move = - (360.-angle_to_move)
+            steps = self._steps_per_rev * angle_to_move / 360
+            self._angle = angle
+            self._move(int(abs(round(steps))), steps>0)
+
+    def _move(self, steps, direction):
+        GPIO.output(self.DIR, direction)
+        for step in xrange(steps):
+            GPIO.output(self.PUL, True)
+            GPIO.output(self.PUL, False)
+            time.sleep(.05)
+
+if __name__ == "__main__":
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    motor = Motor([15,14])
+    motor.move_to(90)
+    motor.move_to(280)
+    motor.move_to(0)
+
---
+++ telescope.py
@@ -0,0 +1,164 @@
+#! /usr/bin/env python
+import SocketServer
+import sys
+import argparse
+import logging
+from datetime import datetime
+from threading import Thread
+from string import replace
+from time import time, sleep
+from bitstring import BitArray, BitStream, ConstBitStream
+import ephem
+import RPi.GPIO as GPIO
+from motor import Motor
+
+def getopts():
+    parser = argparse.ArgumentParser(description="Telescope Server")
+    parser.add_argument("--host", default="0.0.0.0")
+    parser.add_argument("--port", type=int, default=10000)
+
+    parser.add_argument("--azimuth-pins", default="15,14,19",
+                        help="GPIO-Pins for azimuthal motor (PUL, DIR, ENBL)")
+    parser.add_argument("--altitude-pins", default="18,20,22",
+                        help="GPIO-Pins for altitudal motor (PUL, DIR, ENBL)")
+    parser.add_argument("--log-level", default="DEBUG",
+                        help="set logging level")
+    args = parser.parse_args(sys.argv[1:])
+    return vars(args)
+
+class TelescopeRequestHandler(SocketServer.BaseRequestHandler):
+    def _stellarium2coords(self, ra_uint, dec_int):
+        return (ra_uint*12./2147483648, dec_int*90./1073741824)
+
+    def _coords2stellarium(self, ra, dec):
+        return (int(ra*(2147483648/12.0)), int(dec*(1073741824/90.0)))
+
+    def handle_stellarium(self, data):
+        if self.server.ready:
+            mtime = data.read('intle:64')
+
+            # RA:
+            ant_pos = data.bitpos
+            ra = data.read('hex:32')
+            data.bitpos = ant_pos
+            ra_uint = data.read('uintle:32')
+
+            # DEC:
+            ant_pos = data.bitpos
+            dec = data.read('hex:32')
+            data.bitpos = ant_pos
+            dec_int = data.read('intle:32')
+
+            ra, dec = self._stellarium2coords(ra_uint, dec_int)
+            logging.info("goto: ra: %f, dec: %f", ra, dec, extra=self.extra)
+
+            self.server.object._ra = "%f" % ra
+            self.server.object._dec = "%f" % dec
+            self.server.observer.date = datetime.utcnow()
+
+            self.server.object.compute(self.server.observer)
+            az = self.server.object.az / ephem.degree
+            alt = self.server.object.alt / ephem.degree
+            logging.debug("time: %s", self.server.observer.date, extra=self.extra)
+            logging.info("az: %f, alt: %f", az, alt, extra=self.extra)
+
+            self.server.motors["az"].move(az)
+
+        else:
+            logging.info("telescope not calibrated", extra=self.extra)
+
+    def set_observer(self, data):
+        self.server.observer.lon = data.read('floatle:32')
+        self.server.observer.lat = data.read('floatle:32')
+        self.server.observer.elev = data.read('floatle:32')
+        logging.info("set observer: %s / %s / %s",
+                        self.server.observer.lon,
+                        self.server.observer.lat,
+                        self.server.observer.elev,
+                        extra=self.extra)
+
+    def handle(self):
+        self.extra = {'clientip': self.client_address[0]}
+        logging.debug("connection established", extra=self.extra)
+        data0 = self.request.recv(160)
+        if data0:
+            data = ConstBitStream(bytes=data0, length=160)
+            msize = data.read('intle:16')
+            mtype = data.read('intle:16')
+
+            logging.debug("client-type: %d", mtype, extra=self.extra)
+            if mtype == 0:
+                # stellarium telescope client
+                ret = self.handle_stellarium(data)
+            elif mtype == 1:
+                # set observer
+                # LON (4 bytes), LAT (4 bytes), ALT (2 bytes)
+                ret = self.set_observer(data)
+
+
+        # for x in range(10):
+        #     localtime = ConstBitStream(replace('int:64=%r' % time(), '.', ''))
+        #     reply = ConstBitStream('0x1800') + ConstBitStream('0x0000')
+        #     reply += ConstBitStream(intle=localtime.intle, length=64) + ConstBitStream(uintle=ra_uint, length=32)
+        #     reply += ConstBitStream(intle=dec_int, length=32) + ConstBitStream(intle=0, length=32)
+        #     print " sending"
+        #     self.request.send(reply.bytes)
+        #     sleep(.001)
+        # self.request.close()
+
+
+
+class TelescopeServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    # Ctrl-C will cleanly kill all spawned threads
+    daemon_threads = True
+    # much faster rebinding
+    allow_reuse_address = True
+
+    def __init__(self, server_address, motors, RequestHandler):
+        SocketServer.TCPServer.__init__(self, server_address, RequestHandler)
+        self.motors = motors
+        self.object = ephem.FixedBody()
+        self.observer = ephem.Observer()
+
+        # testing
+        self.observer.lon = '15:25:12.0'
+        self.observer.lat = '47:4:48.01'
+        self.observer.elev = 362
+
+        for m in self.motors.values():
+            m.angle=0
+            m.steps_per_rev=800
+
+    @property
+    def ready(self):
+        ret = True
+        for motor in self.motors.values():
+            ret = ret and motor.calibrated
+        ret = ret and self.observer.lat != 0
+        return ret
+
+if __name__ == "__main__":
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+
+    args = getopts()
+
+    # set logging level
+    numeric_level = getattr(logging, args["log_level"].upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % loglevel)
+
+    logging.basicConfig(filename="telescope.log",
+                        level=numeric_level,
+                        format="%(asctime)s %(clientip)s %(message)s",
+                        datefmt='%Y-%m-%d %H:%M:%S')
+
+    motors = {"az": Motor("Azimuth motor", map(int, args["azimuth_pins"].split(","))),
+              "alt": Motor("Altitude motor", map(int, args["altitude_pins"].split(",")), max_angle=90)}
+    server = TelescopeServer((args["host"], args["port"]), motors, TelescopeRequestHandler)
+
+    # terminate with Ctrl-C
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        sys.exit(0)
